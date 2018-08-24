a3ff39b777fcc56d043a0a18501228005ed2dca2
cjones4 <cjones4@phoenixunion.org>
2018-08-24 09:03:58 -0700

    Added 2018-08-24

---
+++ 2018-08-21ScreenEvents001.py
@@ -0,0 +1,23 @@
+import turtle # imports the turtle library
+import random # imports the random library
+
+scr = turtle.Screen() # goes into turtle library and calls screen function
+trt = turtle.Turtle() # creates turtle
+scr.tracer(10,0)
+trt.color("black",'blue')
+def littledraw():
+    myx = random.randrange(-200,200)
+    myy = random.randrange(-200,200)
+    randsize = random.randrange(5,20)
+    trt.penup()
+    trt.goto(myx,myy)#sends turtle to random x and
+    trt.pendown()
+    trt.begin_fill()
+    trt.circle(randsize)
+    trt.end_fill()
+
+scr.listen() # readies screen events
+scr.update()# refreshes the screen
+scr.onkey(littledraw, "a")
+scr.mainloop() #keeps screen looping
+
---
+++ to_be_added_04-07-2018/TBA-OOP-2018/pyglettest.py
@@ -0,0 +1,6 @@
+import pyglet
+from pyglet import app
+
+win = pyglet.window.Window()
+
+app.run()
